0c4ab16e09814e76cc166a3c126a1cf6a87405fa
vperalta55 <34689596+vperalta55@users.noreply.github.com>
2024-11-28 14:44:31 -0500

    Add files via upload

---
+++ winget_01.ps1
@@ -0,0 +1,19 @@
+﻿# Inputs
+$app = "Mozilla.Firefox"
+# Query for directory where the most updated winget.exe is stored
+$wingetDir = Get-ChildItem "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe" | Sort-Object -Property LastWriteTime | Select-Object -Last 1
+
+if ($wingetDir) {
+    # Output directory
+    Write-out "cd to directory: $($wingetDir.FullName)"
+
+    # Construct the full path to winget.exe
+    $wingetPath = Join-Path -Path $wingetDir.FullName -ChildPath "winget.exe"
+
+    # Install the specified app using winget.exe
+    & $wingetPath install --id $app --silent --accept-package-agreements --accept-source-agreements
+} 
+else {
+    Write-out "Error: Windows Package Manager (winget) is not installed on the system."
+}
+
